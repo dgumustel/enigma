ab94a19c8a536e45592aba78a003d36fa17794a5
Derya Gumustel <yoshdragon@gmail.com>
2023-03-14 16:29:57 -0700

    Add backwards rotation and set_ring function

--- rotor.py
+++ rotor.py
@@ -20,11 +20,24 @@ class Rotor:
         print(self.right)
         print('')
     
-    def rotate(self, n=0):
+    def rotate(self, n=1, forward=True):
         for i in range(n):
-            self.left = self.left[1:] + self.left[0]
-            self.right = self.right[1:] + self.right[0]
+            if forward:
+                self.left = self.left[1:] + self.left[0]
+                self.right = self.right[1:] + self.right[0]
+            else:
+                self.left = self.left[25] + self.left[:25]
+                self.right = self.right[25] + self.right[:25]
 
     def rotate_to_letter(self, letter):
         n = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.find(letter)
         self.rotate(n)
+
+    def set_ring(self, n):
+        # Rotate the rotor backwards
+        self.rotate(n-1, forward=False)
+
+        # Adjust the turnover notch in relationship to the wiring 
+        # TODO: check that (n_notch - n) % 26 is correct; comments online propose n + 1 or n - 1
+        n_notch = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.find(self.notch)
+        self.notch = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[(n_notch - n) % 26]
